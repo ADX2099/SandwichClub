c134a655404b493867cc171d64151c8e2d499c21
ADX2099 <radagasth@gmail.com>
2019-09-04 10:43:00 -0500

    Fix de ISSUE #1

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -10,7 +10,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
-
+    public static final String KEY_MAIN_NAME = "mainName";
+    public static final String KEY_ALSO_KNOW_AS = "alsoKnownAs";
+    public static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_INGREDIENTS = "ingredients";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
 
 
     public static Sandwich parseSandwichJson(String json) throws JSONException {
@@ -18,22 +24,23 @@ public class JsonUtils {
         List<String> alsoKnownList = new ArrayList<>();
         List<String> ingredientsList = new ArrayList<>();
 
+
         JSONObject sandwichJSON = new JSONObject(json);
-        JSONObject nameValues = sandwichJSON.getJSONObject("name");
+        JSONObject nameValues = sandwichJSON.getJSONObject(KEY_NAME);
 
         Sandwich sandwichDetails = new Sandwich();
-        JSONArray alsoKnownArray = nameValues.getJSONArray("alsoKnownAs");
+        JSONArray alsoKnownArray = nameValues.getJSONArray(KEY_ALSO_KNOW_AS);
         for(int i = 0; i < alsoKnownArray.length(); i++){
             alsoKnownList.add(alsoKnownArray.getString(i) );
         }
-        JSONArray ingredientsArray = sandwichJSON.getJSONArray("ingredients");
+        JSONArray ingredientsArray = sandwichJSON.getJSONArray(KEY_INGREDIENTS);
         for(int i = 0; i < ingredientsArray.length(); i++){
             ingredientsList.add(ingredientsArray.getString(i));
         }
-        name = nameValues.getString("mainName");
-        placeOrigin = sandwichJSON.getString("placeOfOrigin");
-        description = sandwichJSON.getString("description");
-        imageUrl = sandwichJSON.getString("image");
+        name = nameValues.getString(KEY_MAIN_NAME);
+        placeOrigin = sandwichJSON.getString(KEY_PLACE_OF_ORIGIN);
+        description = sandwichJSON.getString(KEY_DESCRIPTION);
+        imageUrl = sandwichJSON.getString(KEY_IMAGE);
 
         sandwichDetails.setPlaceOfOrigin(placeOrigin);
         sandwichDetails.setMainName(name);
